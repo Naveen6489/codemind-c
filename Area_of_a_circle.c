5b917ba12603c573696cba30966cc8faf4b7e782
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-10-13 23:22:36 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main(){     int n;
        cin>>n;
        float k;
        k=3.14*n*n;
        cout<<fixed<<setprecision(2)<<k;
        
    }

---
+++ Area_of_a_circle.c
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    
+    cin>>n;
+    float k;
+    k=3.14*n*n;
+    cout<<fixed<<setprecision(2)<<k;
+    
+}
