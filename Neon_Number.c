533d7ce78f85a2cfdb4cde6e4e9f629cd8246ded
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-10-14 21:59:41 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n,summ,l,k;     cin>>n;     summ=0;     k=n*n;     while(k>0){         l=k%10;         summ+=l;         k=k/10;
        }
        if(summ==n) {
            cout<<"Neon Number";
        }
        else{
            cout<<"Not Neon Number";
        }
    }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,k,l,summ;
+    int n,summ,l,k;
     cin>>n;
     summ=0;
     k=n*n;
     while(k>0){
         l=k%10;
-        summ=summ+l;
+        summ+=l;
         k=k/10;
-    }
-    if(summ==n){
-        cout<<"Neon Number";
         
+    }
+    if(summ==n) {
+        cout<<"Neon Number";
     }
     else{
         cout<<"Not Neon Number";
