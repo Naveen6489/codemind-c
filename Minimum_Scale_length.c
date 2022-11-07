94d03c686266d6b77f74f5fc35507b9a0568e56d
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-11-07 16:51:34 +0530

    #include<bits/stdc++.h> using namespace std; int gcd(int a,int b)     {         if(b==0)         return a;         return gcd(b,a%b);
        }
        int main(){
            int n;
            cin>>n;
            int a[n];
            for(int i=0;i<=n;i++) cin>>a[i];
            for(int i=0;i<=n;i++)a[i+1]=gcd(a[i],a[i+1]);
            cout<<a[n-1];
        }

---
+++ Minimum_Scale_length.c
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int gcd(int a,int b)
+    {
+        if(b==0)
+        return a;
+        return gcd(b,a%b);
+        
+    }
+    int main(){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<=n;i++) cin>>a[i];
+        for(int i=0;i<=n;i++)a[i+1]=gcd(a[i],a[i+1]);
+        cout<<a[n-1];
+    }
