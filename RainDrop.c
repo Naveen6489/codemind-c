2c7fff0ec0f437519f8c744bcc3aa9436467ee50
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-11-09 01:58:09 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin>>n;     if(n%3==0 && n%5==0 && n%7==0) cout<<"PlingPlangPlong";     else if(n%3==0 and n%5==0) cout<<"PlingPlang";     else if(n%3==0) cout<<"Pling";     else if(n%7==0) cout<<"Plong";     else if(n%5==0) cout<<"Plang";     else cout<<n; }

---
+++ RainDrop.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%3==0 && n%5==0 && n%7==0) cout<<"PlingPlangPlong";
+    else if(n%3==0 and n%5==0) cout<<"PlingPlang";
+    else if(n%3==0) cout<<"Pling";
+    else if(n%7==0) cout<<"Plong";
+    else if(n%5==0) cout<<"Plang";
+    else cout<<n;
+}
