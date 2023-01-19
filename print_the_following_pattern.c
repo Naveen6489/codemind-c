414d3f6b00653299a23aac993b6ebe7f24d55c08
M.J.S.SNaveen <102216712+Naveen6489@users.noreply.github.com>
2023-01-19 16:23:44 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin>>n;     for(int i=1;i<=n;i++){         for(int j=1;j<=i;j++){             cout<<j;         }         cout<<endl;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=1;j<=n-i;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
             cout<<j;
         }
         cout<<endl;
